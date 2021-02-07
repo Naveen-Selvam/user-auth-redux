755769b8ec2a0fe64d2152863b3036d700665cc5
naveens <joinnaveens@gmail.com>
2021-02-07 09:16:08 +0530

    updated

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,14 +1,21 @@
 import React from "react";
+import img1 from "../pics/img1.png";
 
 const Home = (props) => {
   let loggedIn = props.location.state;
   return (
     <div>
       {loggedIn && <h6>{props.location.state}</h6>}
-      <h1>Home Component</h1>
+      <h1>User - Authentication</h1>
+
+      <img
+        src={img1}
+        style={{ marginLeft: "4.5rem" }}
+        width="90%"
+        height="515px"
+      />
     </div>
   );
 };
- 
+
 export default Home;
- 
